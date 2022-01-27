9e5f9e99e2e66ace8b1615fc69e2bec1b9fcf492
Adwait <adwait58@students.cs.ubc.ca>
2022-01-27 10:23:50 -0800

    Added project files

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
+
+const validate = [
+  check("email", "Must be a valid email address")
+    .isEmail()
+    .trim()
+    .escape()
+    .normalizeEmail(),
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters")
+    .matches(["0-9"])
+    .withMessage("Password must contain a number")
+    .matches(["A-Z"])
+    .withMessage("Password must contain an uppercase character")
+    .trim()
+    .escape(),
+];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/static"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/static/index.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/static/login.html");
+});
+
+app.post("/login", validate, (req, res) => {
+  const err = validationResult(req);
+  if (!err.isEmpty()) {
+    return res.status(422).json({ err: err.array() });
+  } else {
+    let email = req.body.email;
+    let password = req.body.password;
+    res.send(`Email: ${email} Password: ${password}`);
+  }
+});
+
+app.listen(8000, () => {
+  console.log("listening on port 8000");
+});
